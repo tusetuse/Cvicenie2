93ea015b3d051115dfa4b6781bc869ae38a97295
IvanRonda <ivanronda500@gmail.com>
2024-02-20 10:46:47 +0100

    Add project files.

---
+++ Cvicenie2/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Cvicenie2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.suborToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otvoritToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zavrietToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ulozitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.suborToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(512, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // suborToolStripMenuItem
+            // 
+            this.suborToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.otvoritToolStripMenuItem,
+            this.zavrietToolStripMenuItem,
+            this.ulozitToolStripMenuItem});
+            this.suborToolStripMenuItem.Name = "suborToolStripMenuItem";
+            this.suborToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.suborToolStripMenuItem.Text = "Subor";
+            // 
+            // otvoritToolStripMenuItem
+            // 
+            this.otvoritToolStripMenuItem.Image = global::Cvicenie2.Properties.Resources.Microsoft_Fluentui_Emoji_Flat_Open_File_Folder_Flat_512;
+            this.otvoritToolStripMenuItem.Name = "otvoritToolStripMenuItem";
+            this.otvoritToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.otvoritToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.otvoritToolStripMenuItem.Text = "Otvorit";
+            this.otvoritToolStripMenuItem.Click += new System.EventHandler(this.otvoritToolStripMenuItem_Click);
+            // 
+            // zavrietToolStripMenuItem
+            // 
+            this.zavrietToolStripMenuItem.Image = global::Cvicenie2.Properties.Resources.Custom_Icon_Design_Flatastic_10_Delete_file_512;
+            this.zavrietToolStripMenuItem.Name = "zavrietToolStripMenuItem";
+            this.zavrietToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.zavrietToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.zavrietToolStripMenuItem.Text = "Zavriet";
+            // 
+            // ulozitToolStripMenuItem
+            // 
+            this.ulozitToolStripMenuItem.Image = global::Cvicenie2.Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_document_save_256;
+            this.ulozitToolStripMenuItem.Name = "ulozitToolStripMenuItem";
+            this.ulozitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.ulozitToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.ulozitToolStripMenuItem.Text = "Ulozit";
+            this.ulozitToolStripMenuItem.Click += new System.EventHandler(this.ulozitToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4,
+            this.toolStripButton5,
+            this.toolStripButton6});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(512, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::Cvicenie2.Properties.Resources.Microsoft_Fluentui_Emoji_Flat_Open_File_Folder_Flat_512;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.ToolTipText = "Otvorit subor";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::Cvicenie2.Properties.Resources.Custom_Icon_Design_Flatastic_10_Delete_file_512;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.ToolTipText = "Zatvorit subor";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = global::Cvicenie2.Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_document_save_256;
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "toolStripButton3";
+            this.toolStripButton3.ToolTipText = "Ulozit subor";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = global::Cvicenie2.Properties.Resources.Gakuseisean_Aire_Font_File_Alt_256;
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton4.Text = "toolStripButton4";
+            this.toolStripButton4.ToolTipText = "Zmenit font";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = global::Cvicenie2.Properties.Resources.Itzikgur_My_Seven_Graphics_Painting_256;
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton5.Text = "toolStripButton5";
+            this.toolStripButton5.ToolTipText = "Zmenit farbu pozadia";
+            this.toolStripButton5.Click += new System.EventHandler(this.toolStripButton5_Click);
+            // 
+            // toolStripButton6
+            // 
+            this.toolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton6.Image = global::Cvicenie2.Properties.Resources.Oxygen_Icons_org_Oxygen_Actions_format_text_color_256;
+            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton6.Name = "toolStripButton6";
+            this.toolStripButton6.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton6.Text = "toolStripButton6";
+            this.toolStripButton6.ToolTipText = "Zmenit farbu textu";
+            this.toolStripButton6.Click += new System.EventHandler(this.toolStripButton6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 52);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 289);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(365, 89);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(110, 252);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(295, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 353);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem suborToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otvoritToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zavrietToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ulozitToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ToolStripButton toolStripButton6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Cvicenie2/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cvicenie2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            otvoritToolStripMenuItem.PerformClick();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            zavrietToolStripMenuItem.PerformClick();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            ulozitToolStripMenuItem.PerformClick();
+        }
+
+        private void otvoritToolStripMenuItem_Click(object sender, EventArgs e)//open text file from PC to textbox1
+        {
+            DialogResult dialogResult;
+            dialogResult = MessageBox.Show("Chcete zatvorit program?", "Aplikacia", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                {
+                    openFileDialog.InitialDirectory = "c:\\";
+                    openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+                    if (openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string fileContent = System.IO.File.ReadAllText(openFileDialog.FileName);
+                        textBox1.Text = fileContent;
+                    }
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        private void ulozitToolStripMenuItem_Click(object sender, EventArgs e)//save file text file from textbox1 to PC
+        {
+            DialogResult dialogResult;
+            dialogResult = MessageBox.Show("Chcete zatvorit program?", "System", MessageBoxButtons.YesNoCancel,MessageBoxIcon.Exclamation);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if(dialogResult == DialogResult.No)
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
+                    saveFileDialog.DefaultExt = "txt";
+                    saveFileDialog.AddExtension = true;
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        System.IO.File.WriteAllText(saveFileDialog.FileName, textBox1.Text);
+                        MessageBox.Show("File saved successfully at " + saveFileDialog.FileName, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)//font tool strip dialog
+        {
+            FontDialog fontDialog = new FontDialog();
+            fontDialog.Font = textBox1.Font;
+            if(fontDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Font = fontDialog.Font;
+            }
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)//color background tool strip dialog
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.Color = textBox1.BackColor;
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.BackColor = colorDialog.Color;
+            }
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)//color text tool strip dialog
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.Color = textBox1.BackColor;
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.ForeColor = colorDialog.Color;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)//factorial calculation from textbox2 to label1
+        {
+            double vstup, faktorial;
+            try
+            {
+                if (textBox2.Text != "")
+                {
+                    vstup = Convert.ToDouble(textBox2.Text);
+                    faktorial = vstup;
+                    while(vstup > 1)
+                    {
+                        faktorial *= --vstup;
+                    }
+                    label1.Text = faktorial.ToString();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Zadal si kokotinu");       
+            }
+        }
+    }
+}
